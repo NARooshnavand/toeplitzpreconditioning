38c118af1256151790fb7daea15283acc2468e9b
NARooshnavand <nasserakhoundi@gmail.com>
2024-06-08 12:13:29 +0330

    add firt function

---
+++ fractionaldiffusion/SgammaM.m
@@ -0,0 +1,9 @@
+function S = SgammaM(g,M)
+//Create the S_{\gamma, M} described in Page 4
+//1- g must belong to (1,2) and M is nonnegative integer 
+s = zeros(M,1);
+s[1] = gamma(g+1)/gamma(g/2+1);//s[1]=s_{0,\gamma}
+for k=2:M 
+    s[k] = (1-(g+1)/(g/2+k+1))*s[k-1]//s[k] = s_{k+1,\gamma} computation gets from the definiation page 20
+end
+S = toeplitz(s);
